286607b538de3c94fd9f3458581a33e1ab392b21
Paul Abreu <Paul.Abreu@parks.nyc.gov>
2022-04-15 15:49:50 -0400

    commit

--- src/app/templates-demo/templates-demo.component.ts
+++ src/app/templates-demo/templates-demo.component.ts
@@ -13,31 +13,36 @@ import * as productsJson from '../../assets/data/products.json';
 })
 export class TemplatesDemoComponent implements OnInit, AfterViewInit {
   //@ViewChild(TemplateRef) "wo_platesTemplate";
-  @ViewChild("wo_platesTemplate") wo_platesTemplateRef;
+  @ViewChild("defaultTemplate") defaultTemplateRef:TemplateRef<any>;
+  @ViewChild("docservicesOptions") docservicesOptionsTemplateRef:TemplateRef<any>;
 
   public categories:any = [];
   public products:SelectItem[] = [];
   public productsAll:any=[];
   
-  public displayTemplate:string = '';
+  public displayCBGOptions:boolean = false;
+  public displayDocServiceOptions:boolean = false;
+  public displayTemplate:TemplateRef<any> = null;
 
   public parentFG:FormGroup = null;
-
-  public XXX = 'wo_platesTemplate';
+  public docservicesFG:FormGroup = null;
+  public optsYN:SelectItem[] = [ {value:'no', label:'No'}, {value:'yes', label:'Yes'}];
+  public optsYNOther:SelectItem[] = [ {value:'no', label:'No'}, {value:'yes', label:'Yes'}, {value:'other', label:'Other'}];
 
   constructor(
     private fb: FormBuilder,
   ) { }
   ngOnInit(): void {
     this._init();
+    this.displayTemplate = this.defaultTemplateRef;
   }
   ngAfterViewInit(): void {
-    console.log('%cTemplatesDemoComponent', 'background-color:yellow;color:black', this.wo_platesTemplateRef);
   }
   
 // ============================================================================
   onSubmit(evt) {
-    console.log('onSubmit:', evt);
+    //console.log('onSubmit:', evt);
+    console.log('onSubmit:', this.parentFG);
   }
 
   selectCategory(evt:SelectItem) {
@@ -45,6 +50,11 @@ export class TemplatesDemoComponent implements OnInit, AfterViewInit {
     console.log('selectCategory:', evt);
 
     this.products = this.productsAll.filter(e => e.ticket_cat_id === catId);
+
+    if(catId === 12)
+      this.displayTemplate = this.docservicesOptionsTemplateRef;
+    else
+      this.displayTemplate = this.defaultTemplateRef;
   }
   selectProduct(evt:SelectItem) {
     console.log('selectProduct:', evt);
@@ -55,9 +65,20 @@ export class TemplatesDemoComponent implements OnInit, AfterViewInit {
     this.productsAll = (productsJson as any).default;
     console.log(this.categories);
 
+    this.docservicesFG = this.fb.group( {
+      'fc_dsCBGContractNo': [''],
+      'fc_dsIsFullSize': ['', [Validators.required] ],
+      'fc_dsIsDoubleSided': [''],
+      'fc_dsIsBound': ['', ],
+      'fc_dsIsStapled': [''],
+      'fc_dsIsColorScan': ['', ],
+      'fc_dsNumCopies': [1, Validators.min(1)]
+    });
+
     this.parentFG = this.fb.group({
       'fc_username': ['', [Validators.required]],
       'fc_email': ['' ],
+      'docservices': this.docservicesFG
     });
   }
 }
